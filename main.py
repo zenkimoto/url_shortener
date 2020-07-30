c013977ac1f5e42aa5dbecf72ccafa7d1bcef12b
Alex Yip <alexyip123@gmail.com>
2020-07-30 06:55:53 -0700

    Added index.html template

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
 
 @app.route('/<hash>')
 def handle_hash(hash):
