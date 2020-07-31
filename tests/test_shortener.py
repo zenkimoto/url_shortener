1ec815efdfd602f814e3252660e41c94fc0b4e95
Alex Yip <alexyip123@gmail.com>
2020-07-31 05:23:00 -0700

    Added additional tests

--- tests/test_shortener.py
+++ tests/test_shortener.py
@@ -1,8 +1,14 @@
 
-from src.convert import convert_to_short_url
+from src.shortener import *
 
-def test_answer():
+def test_convert_single_digit_number_to_short_url():
     assert convert_to_short_url(3) == 'd'
 
-def test_answer2():
+def test_multiple_digit_number_to_short_url():
     assert convert_to_short_url(8000) == 'cfc'
+
+def test_convert_single_character_to_id():
+    assert convert_to_id('d') == 3
+
+def test_multiple_character_string_to_id():
+    assert convert_to_id('cfc') == 8000
