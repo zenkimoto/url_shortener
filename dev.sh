85619822d3d0bbf1f005b9caa9617de8a1d57519
Alex Yip <alexyip123@gmail.com>
2020-08-01 07:36:17 -0700

    Added virtual environment activate to dev and prod scripts

--- dev.sh
+++ dev.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source venv/bin/activate
+
 export FLASK_ENV=development
 export FLASK_APP=src/main.py
 flask run
--- prod.sh
+++ prod.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source venv/bin/activate
+
 export FLASK_ENV=production
 export FLASK_APP=src/main.py
 flask run --host=0.0.0.0
