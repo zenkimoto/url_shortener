d72fed5c2a3f7cb39b06186dbf6afe8869b7a19a
Alex Yip <alexyip123@gmail.com>
2020-08-02 06:33:46 -0700

    Added comments and additional tests and fixed a bug

--- src/shortener.py
+++ src/shortener.py
@@ -1,44 +1,66 @@
 from math import trunc
 
+URL_SHORTENER_BASE = 62
+ASCII_LOWERCASE_A = 97
+ASCII_UPPERCASE_A = 65
+ASCII_NUMBER_0 = 48
+
 def convert_to_short_url(num):
-    # a - 97 - 122 => 26
-    # A - 65 - 90 => 26
-    # 0 - 48 - 57 => 10
-    # a - 97 - 122
-    # A - 65 - 90
-    # 0 - 48 - 57
-    # This function will perform a bijection from a number to a string 0-9a-zA-Z
+    # Convert number to base 62 where:
+    #   0  => a  (ASCII 97)
+    #   10 => k
+    #   25 => z
+    #   26 => A  (ASCII 65)
+    #   51 => Z
+    #   52 => 0  (ASCII 48)
+    #   61 => 9
+    #   62 => ba
+    #   63 => bb
+
     result = ''
 
     while True:
-        rem = num % 62
+        rem = num % URL_SHORTENER_BASE
 
+        # Take ASCII # + remainder and concatenate
         if rem < 26:
-            result = chr(97 + rem) + result         # a-z
+            result = chr(ASCII_LOWERCASE_A + rem) + result         # a-z
         elif rem < 52:
-            result = chr(65 + rem - 26) + result    # A-Z
+            result = chr(ASCII_UPPERCASE_A + rem - 26) + result    # A-Z
         else:
-            result = chr(48 + rem - 52) + result    # 0-9
+            result = chr(ASCII_NUMBER_0 + rem - 52) + result       # 0-9
 
-        num = trunc(num / 62)
+        num = trunc(num / URL_SHORTENER_BASE)
         if num <= 0:
             break;
 
     return result
 
 def convert_to_id(string):
+    # Converts base 62 string to base 10 int
+    #   a  => 0   (ASCII 97)
+    #   k  => 10
+    #   z  => 25
+    #   A  => 26  (ASCII 65)
+    #   Z  => 51
+    #   0  => 52  (ASCII 48)
+    #   9  => 61
+    #   ba => 62
+    #   bb => 63
+
     result = 0
 
     while len(string) > 0:
         ch = string[0]
 
         if ord('a') <= ord(ch) <= ord('z'):
-            result = result * 62 + ord(ch) - 97
+            result = result * URL_SHORTENER_BASE + ord(ch) - ASCII_LOWERCASE_A
         elif ord('A') <= ord(ch) <= ord('Z'):
-            result = result * 62 + ord(ch) - 65
+            result = result * URL_SHORTENER_BASE + ord(ch) - ASCII_UPPERCASE_A + 26
         else:
-            result = result * 62 + ord(ch) - 48
+            result = result * URL_SHORTENER_BASE + ord(ch) - ASCII_NUMBER_0 + 52
 
+        # Discard first character
         string = string[1:]
 
     return result
--- tests/test_shortener.py
+++ tests/test_shortener.py
@@ -1,14 +1,43 @@
 
 from src.shortener import *
 
-def test_convert_single_digit_number_to_short_url():
-    assert convert_to_short_url(3) == 'd'
+def test_convert_zero_to_lowercase_a():
+    assert convert_to_short_url(0) == 'a'
 
-def test_multiple_digit_number_to_short_url():
+def test_convert_two_digit_number_to_short_url():
+    assert convert_to_short_url(10) == 'k'
+
+def test_transition_from_lowercase_to_uppercase():
+    assert convert_to_short_url(25) == 'z'
+    assert convert_to_short_url(26) == 'A'
+
+def test_transition_from_uppercase_to_number():
+    assert convert_to_short_url(51) == 'Z'
+    assert convert_to_short_url(52) == '0'
+
+def test_transition_from_number_to_two_letter():
+    assert convert_to_short_url(61) == '9'
+    assert convert_to_short_url(62) == 'ba'
+    assert convert_to_short_url(63) == 'bb'
+
+def test_convert_larger_digit_number_to_short_url():
     assert convert_to_short_url(8000) == 'cfc'
 
-def test_convert_single_character_to_id():
-    assert convert_to_id('d') == 3
+def test_convert_a_character_to_0():
+    assert convert_to_id('a') == 0
+
+def test_transition_from_lowercase_to_uppercase_to_id():
+    assert convert_to_id('z') == 25
+    assert convert_to_id('A') == 26
+
+def test_transition_from_uppercase_to_number_to_id():
+    assert convert_to_id('Z') == 51
+    assert convert_to_id('0') == 52
+
+def test_transition_from_number_to_two_digit_to_id():
+    assert convert_to_id('9')  == 61
+    assert convert_to_id('ba') == 62
+    assert convert_to_id('bb') == 63
 
 def test_multiple_character_string_to_id():
     assert convert_to_id('cfc') == 8000
